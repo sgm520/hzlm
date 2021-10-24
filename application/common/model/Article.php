bdc95089e0eb3a96308354ee9977e5359892344b
sgm520 <982018414@qq.com>
2021-10-24 10:11:14 +0800

    '上传'

--- application/common/model/Article.php
+++ application/common/model/Article.php
@@ -18,15 +18,11 @@ class Article extends Model
     protected $updateTime = 'update_time';
     // 追加属性
     protected $append = [
-        'create_time_text'
     ];
 
     public function category(){
        return $this->belongsTo(ArticleCategory::class,'category','id',[],'LEFT')->setEagerlyType(0);
     }
 
-    public function getCreateTimeTextAttr($name)
-    {
-        return date('Y-m-d h:i:s', $this->getAttr('create_time'));
-    }
+
 }
