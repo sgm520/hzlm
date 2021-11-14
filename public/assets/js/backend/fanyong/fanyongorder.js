9fb3f160e45833092df97fbd7d1a40327cea1bc1
sgm520 <982018414@qq.com>
2021-11-14 11:52:52 +0800

    '去掉'

--- public/assets/js/backend/fanyong/fanyongorder.js
+++ public/assets/js/backend/fanyong/fanyongorder.js
@@ -72,7 +72,7 @@ define(['jquery', 'bootstrap', 'backend', 'csmtable', 'form','fixedcolumns','tab
                 {field: 'json.tu2', title: __('示例图2'),operate:false,events: Table.api.events.image, formatter: Table.api.formatter.image, operate: false},
                 {field: 'json.tu3', title: __('示例图3'),operate:false,events: Table.api.events.image, formatter: Table.api.formatter.image, operate: false},
                 // {field: 'xlines', title: __('下款额度'),operate: false},
-                {field: 'fmoney', title: __('返佣金额'),operate: false,showsum:true},
+                {field: 'fmoney', title: __('返佣金额'),operate: false},
 
                 {field: 'user_ip', title: __('ip'),operate: false},
                 {field: 'ment', title: __('设备'),operate: false},
