7ae1016951806846abfdbecca5a2fba69fbefbd6
sgm520 <982018414@qq.com>
2021-11-21 21:18:38 +0800

    'a'

--- application/admin/controller/MyChild.php
+++ application/admin/controller/MyChild.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use app\common\controller\Backend;
 use app\common\library\Auth;
+use think\Db;
 
 /**
  * 会员管理
@@ -39,12 +40,11 @@ class MyChild extends Backend
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
-
+            $parnet= Db::name('user')->where('id',session('ids'))->find();
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
             $list = $this->model
-                ->with('group')
                 ->where($where)
-                ->where('parent_id',session('ids'))
+                ->where('parent_id',$parnet['invite_code'])
                 ->order($sort, $order)
                 ->paginate($limit);
             foreach ($list as $k => $v) {
